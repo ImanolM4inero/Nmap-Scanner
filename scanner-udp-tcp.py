ad36d53f139d0fc1ecbcbec60984fe4256c252c0
Iñaki Recabarren <83521709+ImanolM4inero@users.noreply.github.com>
2023-06-07 14:20:28 -0300

    Update scanner-udp-tcp.py

--- scanner-udp-tcp.py
+++ scanner-udp-tcp.py
@@ -1,2 +1,21 @@
+print('Bienvenido a mi primer programa de escaneo')
+
 import nmap
 
+ip_ad = input('INGRESE LA IP A ESCANEAR: ')
+
+scanner = nmap.PortScanner()
+
+print('LA IP QUE INGRESO ES LA ', ip_add)
+type(ip_add)
+
+respuesta = input('''\n Por favor seleccione el tipo de escaneo
+  1) Puertos TCP
+  2) Puertos UDP \n''')
+
+print('Su seleccion fue: ', respuesta)
+
+if respuesta =='1':
+  print('Nmap version: ', scanner.nmap_version())
+  scanner.scann(ip_add, '1-1024', '-O', '-v')
+  print(scanner.scaninfo())
